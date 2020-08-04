20b7b53fb1795b9de7e40d7c6de14833f7ee65ad
koji-endo <skyclear.288@gmail.com>
2020-08-05 06:32:45 +0900

    lab5の変更

--- pru-software-support-package/labs/lab_5/solution/PRU_RPMsg_Echo_Interrupt1/main.c
+++ pru-software-support-package/labs/lab_5/solution/PRU_RPMsg_Echo_Interrupt1/main.c
@@ -31,25 +31,25 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <stdint.h>
-#include <stdio.h>
+#include "resource_table_1.h"
 #include <pru_cfg.h>
 #include <pru_intc.h>
-#include <rsc_types.h>
 #include <pru_rpmsg.h>
-#include "resource_table_1.h"
+#include <rsc_types.h>
+#include <stdint.h>
+#include <stdio.h>
 
 volatile register uint32_t __R31;
 
 /* Host-1 Interrupt sets bit 31 in register R31 */
-#define HOST_INT			((uint32_t) 1 << 31)	
+#define HOST_INT ((uint32_t)1 << 31)
 
-/* The PRU-ICSS system events used for RPMsg are defined in the Linux device tree
- * PRU0 uses system event 16 (To ARM) and 17 (From ARM)
- * PRU1 uses system event 18 (To ARM) and 19 (From ARM)
+/* The PRU-ICSS system events used for RPMsg are defined in the Linux device
+ * tree PRU0 uses system event 16 (To ARM) and 17 (From ARM) PRU1 uses system
+ * event 18 (To ARM) and 19 (From ARM)
  */
-#define TO_ARM_HOST			18	
-#define FROM_ARM_HOST			19
+#define TO_ARM_HOST 18
+#define FROM_ARM_HOST 19
 
 /*
  * Using the name 'rpmsg-client-sample' will probe the RPMsg sample driver
@@ -58,54 +58,63 @@ volatile register uint32_t __R31;
  * Using the name 'rpmsg-pru' will probe the rpmsg_pru driver found
  * at linux-x.y.z/drivers/rpmsg/rpmsg_pru.c
  */
-#define CHAN_NAME			"rpmsg-client-sample"
-//#define CHAN_NAME			"rpmsg-pru"
+//#define CHAN_NAME			"rpmsg-client-sample"
+#define CHAN_NAME "rpmsg-pru"
 
-#define CHAN_DESC			"Channel 31"
-#define CHAN_PORT			31
+#define CHAN_DESC "Channel 31"
+#define CHAN_PORT 31
 
 /*
  * Used to make sure the Linux drivers are ready for RPMsg communication
  * Found at linux-x.y.z/include/uapi/linux/virtio_config.h
  */
-#define VIRTIO_CONFIG_S_DRIVER_OK	4
+#define VIRTIO_CONFIG_S_DRIVER_OK 4
 
 uint8_t payload[RPMSG_BUF_SIZE];
 
 /*
  * main.c
  */
-void main(void)
-{
-	struct pru_rpmsg_transport transport;
-	uint16_t src, dst, len;
-	volatile uint8_t *status;
+void main(void) {
+  struct pru_rpmsg_transport transport;
+  uint16_t src, dst, len;
+  volatile uint8_t *status;
 
-	/* Allow OCP master port access by the PRU so the PRU can read external memories */
-	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+  /* Allow OCP master port access by the PRU so the PRU can read external
+   * memories */
+  CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
-	/* Clear the status of the PRU-ICSS system event that the ARM will use to 'kick' us */
-	CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
+  /* Clear the status of the PRU-ICSS system event that the ARM will use to
+   * 'kick' us */
+  CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
 
-	/* Make sure the Linux drivers are ready for RPMsg communication */
-	status = &resourceTable.rpmsg_vdev.status;
-	while (!(*status & VIRTIO_CONFIG_S_DRIVER_OK));
+  /* Make sure the Linux drivers are ready for RPMsg communication */
+  status = &resourceTable.rpmsg_vdev.status;
+  while (!(*status & VIRTIO_CONFIG_S_DRIVER_OK))
+    ;
 
-	/* Initialize the RPMsg transport structure */
-	pru_rpmsg_init(&transport, &resourceTable.rpmsg_vring0, &resourceTable.rpmsg_vring1, TO_ARM_HOST, FROM_ARM_HOST);
+  /* Initialize the RPMsg transport structure */
+  pru_rpmsg_init(&transport, &resourceTable.rpmsg_vring0,
+                 &resourceTable.rpmsg_vring1, TO_ARM_HOST, FROM_ARM_HOST);
 
-	/* Create the RPMsg channel between the PRU and ARM user space using the transport structure. */
-	while (pru_rpmsg_channel(RPMSG_NS_CREATE, &transport, CHAN_NAME, CHAN_DESC, CHAN_PORT) != PRU_RPMSG_SUCCESS);
-	while (1) {
-		/* Check bit 30 of register R31 to see if the ARM has kicked us */
-		if (__R31 & HOST_INT) {
-			/* Clear the event status */
-			CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
-			/* Receive all available messages, multiple messages can be sent per kick */
-			while (pru_rpmsg_receive(&transport, &src, &dst, payload, &len) == PRU_RPMSG_SUCCESS) {
-				/* Echo the message back to the same address from which we just received */
-				pru_rpmsg_send(&transport, dst, src, payload, len);
-			}
-		}
-	}
+  /* Create the RPMsg channel between the PRU and ARM user space using the
+   * transport structure. */
+  while (pru_rpmsg_channel(RPMSG_NS_CREATE, &transport, CHAN_NAME, CHAN_DESC,
+                           CHAN_PORT) != PRU_RPMSG_SUCCESS)
+    ;
+  while (1) {
+    /* Check bit 30 of register R31 to see if the ARM has kicked us */
+    if (__R31 & HOST_INT) {
+      /* Clear the event status */
+      CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
+      /* Receive all available messages, multiple messages can be sent per kick
+       */
+      while (pru_rpmsg_receive(&transport, &src, &dst, payload, &len) ==
+             PRU_RPMSG_SUCCESS) {
+        /* Echo the message back to the same address from which we just received
+         */
+        pru_rpmsg_send(&transport, dst, src, payload, len);
+      }
+    }
+  }
 }
